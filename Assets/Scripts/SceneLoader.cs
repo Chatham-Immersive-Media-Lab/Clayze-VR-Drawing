f95a976df750826661a42c49a81131fdd2b8f810
Hunter Dyar <bloop@hdyar.com>
2024-08-16 14:26:29 -0400

    scene loader untested!

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utilities;
+public class SceneLoader : MonoBehaviour
+{
+    [ScenePath]
+    public string Scene;
+
+    public bool LoadOnStart;
+
+    void Start()
+    {
+        if (LoadOnStart)
+        {
+            LoadScene();
+        }
+    }
+
+    private void LoadScene()
+    {
+        //path not name?
+        SceneManager.LoadScene(Scene);
+    }
+}
